23158c1163e2e63510d4c60d70f901cd56c06657
Lukasz <lukasztecza.pl@gmail.com>
2017-03-28 22:12:44 -0400

    Create common_install.sh

---
+++ bash/common_install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#LIGHT DEV ENV
+
+#after getting lubuntu minimal installation from
+#https://help.ubuntu.com/community/Lubuntu/Documentation/MinimalInstall
+#and installing lubuntu-desktop, standard-system-utilities
+
+# install vim, filezilla, chromium, virtualbox and vagrant
+sudo apt-get update
+sudo apt-get install -y vim filezilla chromium-browser virtualbox vagrant php-cli git composer
+
+# install skype and adobre reader
+sudo add-apt-repository "deb http://archive.canonical.com/ $(lsb_release -sc) partner"
+sudo apt-get update
+sudo apt-get install -y skype adobereader-enu
+sudo add-apt-repository -r "deb http://archive.canonical.com/ $(lsb_release -sc) partner"
