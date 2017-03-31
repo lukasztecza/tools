0eae7c2216d57982395f28075de93bb38f9ec709
lukasztecza <lukasztecza.pl@gmail.com>
2017-03-30 23:48:01 -0400

    update bootstrap htaccess stuff

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -17,7 +17,7 @@ apt-get update
 apt-get install -y vim curl
 
 # Install apache and create symlink pointing default apache web dir to /vagrant
-apt-get install -y apache2=$APACHE_VERSION
+apt-get install -y apache2="$APACHE_VERSION"
 
 # Create symlink from default apache web dir to /vagrant
 if ! [ -L /var/www/html ]; then
@@ -28,13 +28,13 @@ fi
 
 # Set ServerName to fix "AH00558: apache2: Could not reliably determine..." error
 if ! fgrep ServerName /etc/apache2/apache2.conf; then
-    echo 'ServerName localhost' | sudo tee -a /etc/apache2/apache2.conf
+    echo "ServerName localhost" | sudo tee -a /etc/apache2/apache2.conf
 fi
 
 # Set mysql answers and install mysql-server
 debconf-set-selections <<< "mysql-server mysql-server/root_password password $MYSQL_ROOT_PASSWORD"
 debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $MYSQL_ROOT_PASSWORD"
-apt-get install -y mysql-server-$MYSQL_VERSION mysql-client-$MYSQL_VERSION
+apt-get install -y mysql-server-"$MYSQL_VERSION" mysql-client-"$MYSQL_VERSION"
 
 # Set key_buffer_size to fix "Using unique option prefix key_buffer instead of key_buffer_size..." warning
 if ! fgrep key_buffer_size /etc/mysql/my.cnf; then
@@ -42,14 +42,52 @@ if ! fgrep key_buffer_size /etc/mysql/my.cnf; then
 fi
 
 # Install mysql-client
-apt-get install -y mysql-client-$MYSQL_VERSION
+apt-get install -y mysql-client-"$MYSQL_VERSION"
 
 # Install php and modules
-apt-get install -y php$PHP_VERSION php-curl php-mysql
+apt-get install -y php"$PHP_VERSION" php-curl php-mysql
 
 # Display all errors for php
-sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/$PHP_VERSION/apache2/php.ini
-sed -i "s/display_errors = .*/display_errors = On/" /etc/php/$PHP_VERSION/apache2/php.ini
+sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/"$PHP_VERSION"/apache2/php.ini
+sed -i "s/display_errors = .*/display_errors = On/" /etc/php/"$PHP_VERSION"/apache2/php.ini
+
+# Allow usage of .htaccess files inside /var/www/html
+cat <<EOL >> /etc/apache2/apache2.conf
+<Directory /var/www/html>
+    Options Indexes FollowSymLinks
+    AllowOverride All
+    Require all granted
+</Directory>
+EOL
+
+# Create .htaccess file
+cat > /vagrant/.htaccess <<EOL
+# Protect .htaccess and .htpasswd files
+<FilesMatch "^(.htaccess|.htpasswd)$">
+    Order Deny,Allow
+    Deny from All
+</FilesMatch>
+
+# Disable directory listing
+Options -Indexes
+
+# Require authentication
+AuthType Basic
+AuthUserFile /vagrant/.htpasswd
+AuthName "Please Authenticate"
+Require valid-user
+EOL
+
+# Create .htpasswd file for dev user:pass (password is encrypted)
+cat > /vagrant/.htpasswd <<EOL
+user:\$apr1\$7joGMP7L\$HdJWcwvWeM9cvoeIK54u1/
+EOL
+
+# Create index.php file with phpinfo
+cat > /vagrant/index.php <<EOL
+<?php
+phpinfo();
+EOL
 
 # Restart apache
 service apache2 restart
